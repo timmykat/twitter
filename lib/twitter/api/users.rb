f402082209a4274b5eaebd8dbe73069ead140dc4
Tim Kinnel <tkinnel@gmail.com>
2013-05-14 17:28:36 -0300

    Remove pry binding - problem solved

--- lib/twitter/api/users.rb
+++ lib/twitter/api/users.rb
@@ -285,7 +285,6 @@ module Twitter
       #     Twitter.user('sferik')
       #     Twitter.user(7505382)  # Same as above
       def user(*args)
-        binding.pry
         arguments = Twitter::API::Arguments.new(args)
         if user = arguments.pop
           merge_user!(arguments.options, user)
